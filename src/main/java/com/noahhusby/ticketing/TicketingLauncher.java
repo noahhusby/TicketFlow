ec3c90c9306731a7b1e7b948ac48ca3423e899b3
Noah Husby <noahhusby@gmail.com>
2022-04-18 09:56:51 -0500

    Added logback for logging

--- src/main/java/com/noahhusby/ticketing/TicketingLauncher.java
+++ src/main/java/com/noahhusby/ticketing/TicketingLauncher.java
@@ -6,16 +6,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class TicketingLauncher extends Application {
+
+    @Getter
+    private final Logger logger = LoggerFactory.getLogger("Ticketing");
+
     @Override
     public void start(Stage stage) throws IOException {
+        logger.info("Starting Ticketing ...");
+
         // Load config
         try {
+            logger.info("Loading configuration ...");
             Configuration configuration = Configuration.of(TicketingConfig.class);
             configuration.sync(TicketingConfig.class);
+            logger.info("Successfully loaded configuration ...");
         } catch (ClassNotConfigException ignored) {
             // I wrote the config library. This exception won't be thrown, so it can be ignored (:
         }
