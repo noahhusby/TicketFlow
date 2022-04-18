0e4bac58630191fe0eafde1a773254ccf752cc92
Noah Husby <noahhusby@gmail.com>
2022-04-18 09:47:05 -0500

    Added config

---
+++ src/main/java/com/noahhusby/ticketing/TicketingConfig.java
@@ -0,0 +1,16 @@
+package com.noahhusby.ticketing;
+
+import com.noahhusby.lib.application.config.Config;
+
+/**
+ * A representation of config options.
+ * The config is automatically handled by <a href="https://github.com/noahhusby/HusbyLib">HusbyLib</a>.
+ *
+ * @author Noah Husby
+ */
+@Config
+public class TicketingConfig {
+    public static String DB_URL = "jdbc:mysql://www.papademas.net:3307/tickets?autoReconnect=true&useSSL=false";
+    public static String DB_USERNAME = "fp411";
+    public static String DB_PASSWORD = "411";
+}
--- src/main/java/com/noahhusby/ticketing/TicketingLauncher.java
+++ src/main/java/com/noahhusby/ticketing/TicketingLauncher.java
@@ -1,5 +1,7 @@
 package com.noahhusby.ticketing;
 
+import com.noahhusby.lib.application.config.Configuration;
+import com.noahhusby.lib.application.config.exception.ClassNotConfigException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +12,14 @@ import java.io.IOException;
 public class TicketingLauncher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Load config
+        try {
+            Configuration configuration = Configuration.of(TicketingConfig.class);
+            configuration.sync(TicketingConfig.class);
+        } catch (ClassNotConfigException ignored) {
+            // I wrote the config library. This exception won't be thrown, so it can be ignored (:
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(TicketingLauncher.class.getResource("ticketing.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
