1460db820d3ea21da004ccffd61a6798b63359be
Noah Husby <noahhusby@gmail.com>
2022-04-18 09:35:48 -0500

    Added Constants.java

---
+++ src/main/java/com/noahhusby/ticketing/Constants.java
@@ -0,0 +1,12 @@
+package com.noahhusby.ticketing;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * A class representing all constants for the project.
+ *
+ * @author Noah Husby
+ */
+@UtilityClass
+public class Constants {
+}
