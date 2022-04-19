ee467cbc1db06dbb3ca3017749b7caa20d01ad34
Noah Husby <noahhusby@gmail.com>
2022-04-19 18:05:01 -0500

    Documentation

--- src/jvmMain/java/com/noahhusby/ticketflow/TicketFlow.java
+++ src/jvmMain/java/com/noahhusby/ticketflow/TicketFlow.java
@@ -1,3 +1,20 @@
+/*
+ * TicketFlow Copyright (C) 2022 Noah Husby
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.noahhusby.ticketflow;
 
 import com.noahhusby.lib.application.config.Configuration;
@@ -6,11 +23,19 @@ import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The primary class representing the backend of the program.
+ *
+ * @author Noah Husby
+ */
 public class TicketFlow {
 
     @Getter
     private static final Logger logger = LoggerFactory.getLogger("Ticketing");
 
+    /**
+     * Starts the backend
+     */
     public void start() {
         logger.info("Starting TicketFlow ...");
 
@@ -25,6 +50,11 @@ public class TicketFlow {
         }
     }
 
+    /**
+     * Gets the user handler.
+     *
+     * @return {@link UserHandler}
+     */
     public UserHandler getUserHandler() {
         return UserHandler.getInstance();
     }
--- src/jvmMain/java/com/noahhusby/ticketflow/UserHandler.java
+++ src/jvmMain/java/com/noahhusby/ticketflow/UserHandler.java
@@ -1,9 +1,28 @@
+/*
+ * TicketFlow Copyright (C) 2022 Noah Husby
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.noahhusby.ticketflow;
 
 import java.util.concurrent.CompletableFuture;
 import lombok.Getter;
 
 /**
+ * A handler for authenticating, creating, and removing users.
+ *
  * @author Noah Husby
  */
 public class UserHandler {
