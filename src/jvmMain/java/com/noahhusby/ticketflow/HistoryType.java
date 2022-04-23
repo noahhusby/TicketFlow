c29e301f0f65e4a2fe9061a4b49557d6cde7e98d
Noah Husby <noahhusby@gmail.com>
2022-04-23 17:10:23 -0500

    Added HistoryType.java

---
+++ src/jvmMain/java/com/noahhusby/ticketflow/HistoryType.java
@@ -0,0 +1,60 @@
+/*
+ * TicketFlow Copyright (C) 2022 Noah Husby
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.noahhusby.ticketflow;
+
+/**
+ * An enumeration of possible types for historical entries.
+ *
+ * @author Noah Husby
+ */
+public enum HistoryType {
+    /*
+     * Event fired each time a user signs in.
+     */
+    LOGIN,
+
+    /*
+     * Event fired each time a user signs out.
+     */
+    LOGOUT,
+
+    /*
+     * Event fired each time a user is added.
+     */
+    USER_ADDED,
+
+    /*
+     * Event fired each time a user is edited.
+     */
+    USER_EDITED,
+
+    /*
+     * Event fired each time a user is deleted.
+     */
+    USER_DELETED,
+
+    /*
+     * Event fired each time a ticket is opened.
+     */
+    TICKET_OPENED,
+
+    /*
+     * Event fired each time a ticket is closed.
+     */
+    TICKET_CLOSED
+}
