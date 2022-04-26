339ed2b3a92484fc9c76a9ed5552c6c69dc68482
Noah Husby <noahhusby@gmail.com>
2022-04-26 10:40:59 -0500

    Formatted header

--- src/jvmMain/kotlin/com/noahhusby/ticketflow/ui/pages/TicketPage.kt
+++ src/jvmMain/kotlin/com/noahhusby/ticketflow/ui/pages/TicketPage.kt
@@ -18,21 +18,21 @@
 package com.noahhusby.ticketflow.ui.pages
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ConfirmationNumber
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.noahhusby.ticketflow.ui.elements.TicketCell
 import com.noahhusby.ticketflow.ui.theme.surfaceColorAtElevation
+import com.noahhusby.ticketflow.ui.theme.tf_ticket_open
 
 class TicketPage : Page {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -67,7 +67,11 @@ class TicketPage : Page {
                         LazyColumn(Modifier.fillMaxSize()) {
                             item {
                                 Surface(Modifier.fillMaxSize().height(48.dp), color = surfaceColorAtElevation(5.dp), border = BorderStroke(0.01.dp, MaterialTheme.colorScheme.outline)) {
-                                    Text("Test")
+                                    Row(Modifier.fillMaxSize().padding(10.dp)) {
+                                        Column {
+                                            Checkbox(false, onCheckedChange = { }, Modifier.scale(0.75f))
+                                        }
+                                    }
                                 }
                             }
                             item {
